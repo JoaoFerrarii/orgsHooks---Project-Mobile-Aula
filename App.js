80a44201aa534b2779443b7bc683f818906e4e0a
unknown <kolodycadu@gmail.com>
2025-09-11 22:01:47 -0300

    Status 1

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
-export default function App() {
+import Home from "./src/telas/Home";
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, World!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   <View>
+    <Home />
+   </View>
+ );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
