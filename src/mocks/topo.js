06987a70632807675569a871913e208cf6701816
João Victor Ferrari da Silva <jvctrferrari@gmail.com>
2025-09-18 21:26:15 -0300

    feat: Implementa estilizacao Topo

---
+++ src/mocks/topo.js
@@ -0,0 +1,5 @@
+const topo ={
+    boasVindas:"Olá, João!",
+    legenda:"Encontre os melhores produtores.",
+}
+export default topo;
--- src/telas/Home/componentes/Topo.js
+++ src/telas/Home/componentes/Topo.js
@@ -1,14 +1,64 @@
 import React from "react";
-import {Image, View, Text } from "react-native";
-
+import {Image, View, Text,StyleSheet} from "react-native";
 import logo from "../../../assets/logo.png";
+import { ImageBackground } from "react-native-web";
 
-export default function Topo() {
-  return (
-    <View>
-        <Image source={logo} />
-        <Text>Olá, Joáo Ferrari!</Text>
-        <Text>Encontre os melhores produtores.</Text>
-    </View>
-  );
+import { carregaTopo } from "../../../services/carregaDados";
+//import { topo } from"../../../mocks/topo";
+
+class Topo extends React.Component{
+  state = {
+    topo: {
+      boasVindas: "",
+      legenda: "",
+    },
+  };
+
+  atualizaTopo(){
+    const retorno  = carregaTopo();
+    this.setState({topo: retorno});
+  }
+
+  componentDidMount(){
+    this.atualizaTopo();
+  }
+
+  render(){
+    return (
+      <View sytle={estilos.topo}>
+          <Image source={logo} style={estilos.imagem}/>
+          <Text style={estilos.boasVindas}>{this.state.topo.boasVindas}</Text>
+          <Text style={estilos.legenda}>{this.state.topo.legenda}</Text>
+      </View>
+    );
+  }
 }
+
+
+
+
+const estilos = StyleSheet.create({
+  topo: {
+    backgroundcolor:"#F6F6F6",
+    padding: 16,
+  },
+  imagem: {
+    widht: 70,
+    height: 28,
+  },
+  boasVindas:{
+    marginTop: 24,
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight:'bold'
+  },
+  legenda:{
+    fontSize: 16,
+    lineHeight: 16,
+    
+  },
+
+
+});
+
+export default Topo;
